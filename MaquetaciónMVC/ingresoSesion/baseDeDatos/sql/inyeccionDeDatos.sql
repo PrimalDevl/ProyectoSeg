062e99780457723619c271b837eeb847d9f3513a
YisooAoi <143233396+YisooAoi@users.noreply.github.com>
2023-08-30 15:56:03 -0300

    Delete MaquetaciónMVC directory

--- MaquetaciónMVC/ingresoSesion/baseDeDatos/sql/inyeccionDeDatos.sql
+++
@@ -1,5 +0,0 @@
-USE informacion_usuarios;
-INSERT INTO administradores (cedula, nombre, apellido)
-VALUES (55554216, 'Fran', 'Yisoo');
-INSERT INTO empleados (cedula, nombre, apellido)
-VALUES (55554216, 'Fran', 'Fubu');
--- MaquetaciónMVC/pantallaEmpleados/baseDeDatos/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conexion = new mysqli('localhost', 'root', '', 'informacion_clientes');
-if ($conexion->connect_errno) {
-  echo "Hubo un error al conectarse al sitio web.";
-  exit;
-}
-?>
