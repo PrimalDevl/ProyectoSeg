b701ff875cb0ac5f26a194f49a0ae6c310d35a91
YisooAoi <143233396+YisooAoi@users.noreply.github.com>
2023-10-24 16:43:47 -0300

    Add files via upload

---
+++ baseDeDatos/sql/datosDePrueba.sql
@@ -0,0 +1,24 @@
+USE gestion_seguros;
+INSERT INTO clientes (cedula, nombre, comentario) VALUES
+(12345010, 'Jose',     'Comentario; no pude, wwwwwww'),
+(12345020, 'Fernando', 'Comentario; no pude, wwwwwww'),
+(12345030, 'Luis',     'Comentario; no pude, wwwwwww'),
+(12345040, 'Maria',    'Comentario; no pude, wwwwwww'),
+(12345050, 'Nicolas',  'Comentario; no pude, wwwwwww'),
+(12345060, 'Bruno',    'Comentario; no pude, wwwwwww'),
+(12345070, 'Leonardo', 'Comentario; no pude, wwwwwww'),
+(12345080, 'Victoria', 'Comentario; no pude, wwwwwww'),
+(12345090, 'Carlos',   'Comentario; no pude, wwwwwww'),
+(12345100, 'Rafael',   'Comentario; no pude, wwwwwww');
+
+INSERT INTO usuarios (cedula, nombre, comentario) VALUES
+(12345110, 'Alberto',     'Comentario; no pude, wwwwwww'),
+(12345120, 'Jhonatan', 'Comentario; no pude, wwwwwww'),
+(12345130, 'Amanda',     'Comentario; no pude, wwwwwww'),
+(12345140, 'Ivan',    'Comentario; no pude, wwwwwww'),
+(12345150, 'Mateo',  'Comentario; no pude, wwwwwww'),
+(12345160, 'Alexander',    'Comentario; no pude, wwwwwww'),
+(12345170, 'Sebastian', 'Comentario; no pude, wwwwwww'),
+(12345180, 'Celeste', 'Comentario; no pude, wwwwwww'),
+(12345190, 'Camila',   'Comentario; no pude, wwwwwww'),
+(12345200, 'Rojelio',   'Comentario; no pude, wwwwwww');
