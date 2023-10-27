266db28315c13c3457dcd38464f4c5a50e4edb77
YisooAoi <143233396+YisooAoi@users.noreply.github.com>
2023-10-27 16:00:13 -0300

    Add files via upload

---
+++ baseDeDatos/sql/datos de prueba empleados.sql
@@ -0,0 +1,11 @@
+instert into Empleados(ci, nombre, apellido, direccion, telefono) values
+(12345110, 'Alberto', 'Rodriguez', 'Pedro 1992', 094547321),
+(12345120, 'Jhonatan', 'Marino', 'Villa Farre 4242', 095847524),
+(12345130, 'Amanda', 'Silva', 'Monte rey 2321', 096454452),
+(12345140, 'Ivan', 'Carrasco', 'Londres 2257', 096424581),
+(12345150, 'Mateo', 'Brum', 'San Martin 8482', 092147856),
+(12345160, 'Alexander', 'Pereira', 'Villa Costa 1478', 097856258),
+(12345170, 'Sebastian', 'Machado', 'San Luis 2942', 096254741),
+(12345180, 'Celeste', 'Vazquez', 'Mendoza 9384', 093547417),
+(12345190, 'Camila', 'Suarez', 'La Costa 7485', 097415589),
+(12345100, 'Rojelio', 'Di Fortuna', 'Pasaje Diagonal 2485', 094746358);
